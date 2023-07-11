51f81e16033268a8bc815cf4a4d0249f55148325
AHNayef <ahnayef@outlook.com>
2023-07-11 11:52:19 +0600

    Created separate directory for DSA

---
+++ Codes/DSA/insertFirst.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node * insertFirst(struct node *toAdd, struct node *head){
+
+    toAdd->next = head;
+
+    head = toAdd;
+
+    return head;
+
+}
+
+void travel(struct node *ptr){
+
+    while (ptr !=NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr = ptr->next;
+    }
+    
+
+}
+
+
+int main(){
+
+struct node *head;
+struct node *middle;
+struct node *last;
+
+head = (struct node *)malloc(sizeof(struct node));
+middle = (struct node *)malloc(sizeof(struct node));
+last = (struct node *)malloc(sizeof(struct node));
+
+head->data=10;
+middle->data=20;
+last->data=30;
+
+head->next=middle;
+middle->next=last;
+last->next=NULL;
+
+
+struct node *new = (struct node *)malloc(sizeof(struct node));
+
+new->data=50;
+
+
+travel(head);
+
+printf("\nAfter insertion:\n");
+
+travel((insertFirst(new,head)));
+
+
+}
