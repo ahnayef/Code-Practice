933888494a4d9d4d625f636f2c95a69205d9accd
AHNayef <ahnayef@outlook.com>
2023-07-11 11:52:53 +0600

    Formated

--- Codes/DSA/insertFirst.c
+++ Codes/DSA/insertFirst.c
@@ -1,62 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
+struct node
+{
     int data;
     struct node *next;
 };
 
-struct node * insertFirst(struct node *toAdd, struct node *head){
+struct node *insertFirst(struct node *toAdd, struct node *head)
+{
 
     toAdd->next = head;
 
     head = toAdd;
 
     return head;
-
 }
 
-void travel(struct node *ptr){
+void travel(struct node *ptr)
+{
 
-    while (ptr !=NULL)
+    while (ptr != NULL)
     {
-        printf("%d\n",ptr->data);
+        printf("%d\n", ptr->data);
         ptr = ptr->next;
     }
-    
-
 }
 
+int main()
+{
 
-int main(){
+    struct node *head;
+    struct node *middle;
+    struct node *last;
 
-struct node *head;
-struct node *middle;
-struct node *last;
+    head = (struct node *)malloc(sizeof(struct node));
+    middle = (struct node *)malloc(sizeof(struct node));
+    last = (struct node *)malloc(sizeof(struct node));
 
-head = (struct node *)malloc(sizeof(struct node));
-middle = (struct node *)malloc(sizeof(struct node));
-last = (struct node *)malloc(sizeof(struct node));
+    head->data = 10;
+    middle->data = 20;
+    last->data = 30;
 
-head->data=10;
-middle->data=20;
-last->data=30;
+    head->next = middle;
+    middle->next = last;
+    last->next = NULL;
 
-head->next=middle;
-middle->next=last;
-last->next=NULL;
+    struct node *new = (struct node *)malloc(sizeof(struct node));
 
+    new->data = 50;
 
-struct node *new = (struct node *)malloc(sizeof(struct node));
+    travel(head);
 
-new->data=50;
+    printf("\nAfter insertion:\n");
 
-
-travel(head);
-
-printf("\nAfter insertion:\n");
-
-travel((insertFirst(new,head)));
-
-
+    travel((insertFirst(new, head)));
 }
