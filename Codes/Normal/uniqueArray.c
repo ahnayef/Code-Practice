91bf9e6e775962d7a1b639d131a4df8e6bfd9762
AHNayef <ahnayef@outlook.com>
2023-07-12 11:40:05 +0600

    Formated

--- Codes/Normal/uniqueArray.c
+++ Codes/Normal/uniqueArray.c
@@ -1,43 +1,40 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-int main(){
+int main()
+{
 
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    int isUnique=0;
+    int isUnique = 0;
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d", &arr[i]);
     }
-    
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = i+1; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (arr[i] != arr[j])
             {
-                isUnique =0;
+                isUnique = 0;
                 break;
-                
-            }else{
-                isUnique=1;
             }
-            
+            else
+            {
+                isUnique = 1;
+            }
         }
-        
     }
 
-    if (isUnique ==1)
+    if (isUnique == 1)
     {
         printf("YES");
-    }else{
+    }
+    else
+    {
         printf("NO");
     }
-    
-    
-
-
 }
