befff498e0e2383024f999080d243d5a4cb99c99
AHNayef <ahnayef@outlook.com>
2023-08-02 22:30:25 +0600

    Init

---
+++ Codes/DSA/Class/insertionSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int insertionSort(int arr[], int len)
+{
+
+    // int arr[5] = {5, 3, 1, 2, 4};
+
+    for (int i = 0; i < len; i++)
+    {
+        
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d", arr[i]);
+    }
+}
+
+int main()
+{
+
+    int arr[5] = {5, 3, 1, 2, 4};
+
+    insertionSort(arr, 5);
+
+    return 0;
+}
