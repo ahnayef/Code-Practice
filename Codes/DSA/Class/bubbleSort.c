5a10dbb16366206835acc700a926079e3dbbf371
AHN <89353441+ahnayef@users.noreply.github.com>
2023-09-09 11:51:19 +0600

    Update bubbleSort.c

--- Codes/DSA/Class/bubbleSort.c
+++ Codes/DSA/Class/bubbleSort.c
@@ -3,7 +3,6 @@
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            // Swap if the element found is greater than the next element
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
