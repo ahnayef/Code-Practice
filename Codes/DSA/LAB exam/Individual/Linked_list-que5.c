2edf4b24b988387e309aca4650e1f062423fd334
AHNayef <ahnayef@outlook.com>
2023-10-04 18:35:46 +0600

    Delete first node from a linked list

---
+++ Codes/DSA/LAB exam/Individual/Linked_list-que5.c
@@ -0,0 +1,76 @@
+// Delete the first node from a linked list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    struct node *next;
+    int data;
+};
+
+struct node *head;
+
+void printList()
+{
+    struct node *ref;
+    ref = head;
+
+    while (ref != NULL)
+    {
+        printf("%d ", ref->data);
+        ref = ref->next;
+    }
+}
+
+void deleteFirstNode()
+{
+    head = head->next; // Easy ?
+}
+
+// Efficient way:
+
+void deleteFirstNodeV2()
+{
+    struct node *temp = head;
+    head = head->next;
+    free(temp);
+}
+
+int main()
+{
+    struct node *one = NULL;
+    struct node *two = NULL;
+    struct node *three = NULL;
+    struct node *four = NULL;
+
+    one = malloc(sizeof(struct node));
+    two = malloc(sizeof(struct node));
+    three = malloc(sizeof(struct node));
+    four = malloc(sizeof(struct node));
+
+    one->data = 10;
+    two->data = 20;
+    three->data = 30;
+    four->data = 40;
+
+    one->next = two;
+    two->next = three;
+    three->next = four;
+    four->next = NULL;
+
+    head = one;
+
+    printf("Original liked list:\n"); // Printing to make it easy and understandable for you.
+    printList();
+
+    printf("\nAfter deleting the first node:\n"); // Printing to make it easy and understandable for you.
+    deleteFirstNode();
+    printList();
+
+    printf("\nAfter deleting the first node(efficient way):\n"); // Printing to make it easy and understandable for you.
+    deleteFirstNodeV2();
+    printList();
+
+    return 0;
+}
