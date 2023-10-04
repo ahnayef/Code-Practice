e7f0258745990c8fef5eaad316e57224a100e132
AHNayef <ahnayef@outlook.com>
2023-10-04 18:56:52 +0600

    Linear search que 1 complete

--- Codes/DSA/LAB exam/Individual/LinearSearch_que1.c
+++ Codes/DSA/LAB exam/Individual/LinearSearch_que1.c
@@ -1,8 +1,51 @@
-#include<stdio.h>
+// Find if a specific element exists in an array(given in the question or taken as input) using Linear search.
 
-int main(){
+// NB: I'm assuming the array isn't provided in the question. I will take the array as input.
 
-printf("Hello");
+#include <stdio.h>
 
-return 0;
+int linearSearch(int arr[], int len, int searchItem)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] == searchItem)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+    int len, searchItem;
+
+    printf("No of element:");
+    scanf("%d", &len);
+
+    int arr[len]; // initialize array
+    // Taking array as input:
+    printf("Enter %d elements:", len);
+    for (int i = 0; i < len; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Taking search element as input:
+    printf("Enter the element to search:");
+    scanf("%d", &searchItem);
+
+    int res = linearSearch(arr, len, searchItem);
+
+    if (res != -1)
+    {
+        printf("Item found at index %d", res);
+    }
+    else
+    {
+        printf("Item not found >_<");
+    }
+
+    return 0;
 }
