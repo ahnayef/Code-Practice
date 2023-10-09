95b85bb51ce52192a85bf07c1c811080938b90e9
AHNayef <ahnayef@outlook.com>
2023-10-09 23:22:43 +0600

    Update

--- Codes/DSA/LAB_exam/Individual/stackWithArray-que1.c
+++ Codes/DSA/LAB_exam/Individual/stackWithArray-que1.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define len 5 // Constant
+#define size 5 // Constant
 
-int arr[len]; // Array len = 5
+int stack[size]; // Array size/length = 5
 int top = -1;
+
+int isFull()
+{
+    if (top == size - 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isEmpty()
+{
+    if (top == -1)
+    {
+        return 1;
+    }
+    return 0;
+}
 
 void push(int data)
 {
-    if (top < len - 1)
-    {
-        top++;
-        arr[top] = data;
-        printf("Push: %d\n", data);
-    }
-    else
+
+    if (isFull())
     {
         printf("Stack overflow!\n");
+        return;
     }
+    top++;
+    stack[top] = data;
 }
 
-void pop()
+int pop()
 {
-    if (top > -1)
-    {
-        printf("Pop: %d\n", arr[top]);
-        arr[top] = -1;
-        top--;
-    }
-    else
+
+    if (isEmpty())
     {
         printf("Stack underflow!\n");
+        exit(1);
     }
+    int del_data = stack[top];
+    top--;
+    return del_data;
 }
 
 void TOP() // can't name it 'top' bacause of the global variable 🙃
 {
-    printf("Top: %d\n", arr[top]); // Printing element instead of index. You can return index but you'll have to print it while calling the function. I'll demonstrate it with top2() function.
+    printf("Top: %d\n", stack[top]); // Printing element instead of index. You can return index but you'll have to print it while calling the function. I'll demonstrate it with top2() function.
 }
 
 // Top() - 2nd aproach:
@@ -45,20 +61,41 @@ int top2()
     return top;
 }
 
+void print()
+{
+    for (int i = 0; i <= top; i++)
+    {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
 
-    push(3);
-    push(2);
-    TOP();
-    push(5);
-    TOP();
+    // push(3);
+    // push(2);
+    // TOP();
+    // push(5);
+    // TOP();
+    // pop();
+    // TOP();
+    // pop();
+    // TOP();
+    // pop();
+    // print();
+
+    push(10);
+    push(20);
+    push(30);
     pop();
+    push(40);
+    printf("pop %d\n", pop());
+    print(); // It's not printf
     TOP();
-    pop();
-    TOP();
-    pop();
-    printf("Top %d\n", arr[top2()]); // makes things messy
+    push(50);
+    print(); 
+    printf("Top %d\n", stack[top2()]); // makes things messy
 
     return 0;
 }
