9e53b710122075ee762afc75b1661378ad01dafb
AHNayef <ahnayef@outlook.com>
2024-01-09 23:29:20 +0600

    1807A

---
+++ Codes/CF/1807A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b, c, t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> a >> b >> c;
+
+        if ((a + b) == c)
+        {
+            cout << "+" << endl;
+        }
+        else
+        {
+            cout << "-" << endl;
+        }
+    }
+
+    return 0;
+}
