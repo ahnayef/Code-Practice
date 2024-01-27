bb1f9d477f4dec6e4230261245133bda065943be
AHNayef <ahnayef@outlook.com>
2024-01-27 23:15:36 +0600

    1760B

---
+++ Codes/CF/1760B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int max = s[0];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] > max)
+            {
+                max = s[i];
+            }
+        }
+        cout << max-96 << endl;
+    }
+
+    return 0;
+}
