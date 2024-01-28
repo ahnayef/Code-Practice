a511b83d9c6f63f4b63f59cf3bbb9ff5fc03f3c0
AHNayef <ahnayef@outlook.com>
2024-01-28 23:12:18 +0600

    702A

--- Codes/CF/702A.cpp
+++ Codes/CF/702A.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n, subCount = 0, subLength = 1, track = 0;
+    int n, subCount = 0, subCount2 = 0;
     cin >> n;
 
     int *arr = new int[n];
@@ -21,30 +21,32 @@ int main()
 
         for (int i = 0; i < n - 1; i++)
         {
-            if (arr[i] > arr[i + 1])
-            {
-                subLength++;
-            }
-        }
-
-        int *subArr = new int[subLength];
-
-        for (int i = 0; i < n - 1; i++)
-        {
             if (arr[i] < arr[i + 1])
             {
                 subCount++;
             }
             else
             {
-                subArr[track] = subCount;
-                track++;
-                subCount = 0;
+                if (subCount < subCount2)
+                {
+                    subCount = 0;
+                }
+                else
+                {
+                    subCount2 = subCount;
+                    subCount = 0;
+                }
             }
-            subArr[track] = subCount;
         }
 
-        cout << *max_element(subArr, subArr + subLength) + 1;
+        if (subCount2 > subCount)
+        {
+            cout << subCount2 + 1;
+        }
+        else
+        {
+            cout << subCount + 1;
+        }
     }
 
     return 0;
