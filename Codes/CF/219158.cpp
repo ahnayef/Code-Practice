956bee559df5e42827566117bd10efa439da5a8f
AHNayef <ahnayef@outlook.com>
2024-02-16 23:18:10 +0600

    219158

---
+++ Codes/CF/219158.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a1;
+    long long a2;
+    char a3;
+    float a4;
+    double a5;
+
+    cin >> a1;
+    cin >> a2;
+    cin >> a3;
+    cin >> a4;
+    cin >> a5;
+
+    cout << a1 << endl;
+    cout << a2 << endl;
+    cout << a3 << endl;
+    cout << a4 << endl;
+    cout << a5;
+    return 0;
+}
