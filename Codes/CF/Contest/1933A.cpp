667ac49771d922d444ea5063cfe190f42da845ab
AHNayef <ahnayef@outlook.com>
2024-02-27 20:52:14 +0600

    1933A

---
+++ Codes/CF/Contest/1933A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, res = 0;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            res += abs(arr[i]);
+        }
+
+        cout << res << endl;
+    }
+    return 0;
+}
