4ea9c2ea667bb4cc7cc7f0b8d731ebe223c92d1e
AHNayef <ahnayef@outlook.com>
2024-03-04 20:11:17 +0600

    Init

---
+++ Codes/CF/1690A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a, b, c;
+        a = 1;
+        b = c = n / 3;
+
+        cout << b << " " << c << " " << a << endl;
+    }
+    return 0;
+}
