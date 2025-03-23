8c700d6771d827dbba036188e51d32da2382e550
AHNayef <ahnayef@outlook.com>
2025-03-23 16:28:55 +0600

    Update VS Code recommendations and add initial assembly code for character case conversion in task 5

--- Codes/Microprocessor/Assembly/Lab 1/task5.asm
+++ Codes/Microprocessor/Assembly/Lab 1/task5.asm
@@ -1,1 +1,21 @@
 ; Uppercase to Lowercase
+
+.MODEL small
+.STACK 100H
+.DATA
+MSG1 DB "Enter a charecter in uppercase$"
+MSG2 DB "Enter a charecter in lowercase$"
+.CODE
+
+MAIN PROC
+
+    MOV AX, @DATA
+    MOV DS, AX
+
+
+
+    MOV AH, 4CH
+    ITN 21H
+
+ENDP MAIN
+END MAIN
