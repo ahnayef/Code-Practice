8d927390e12c470a1ba8b460462007a24ca09ba2
AHNayef <ahnayef@outlook.com>
2025-03-24 11:19:43 +0600

    Add assembly code for LAB1 and LAB2 tasks, including user input handling and formatted output

---
+++ Codes/Microprocessor/Assembly/Assignments/LAB2/Task1.asm
@@ -0,0 +1,109 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+STAR DB "***********", 0AH, 0DH, "$" 
+STAR2 DB "*****"
+NL DB 0AH, 0DH, "$"
+a DB ?, "$"
+b DB ?, "$"
+c DB ?, "$"
+.CODE
+
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AH, 2
+    MOV DL, "?"
+    INT 21H
+
+    MOV AH, 1
+    INT 21h
+    MOV a, AL
+    INT 21h
+    MOV b, AL
+    INT 21h
+    MOV c, AL
+
+    MOV CX, 4
+    MOV AH, 9
+    LEA DX, NL
+    INT 21H
+label1:
+    LEA DX, STAR
+    INT 21H
+    loop label1
+
+    MOV AH, 2
+    MOV DL, "*"
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    MOV DL, a
+    INT 21H
+    MOV DL, "*"
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+
+    MOV AH, 9
+    LEA DX, NL
+    INT 21H
+
+    MOV AH, 2
+    MOV DL, "*"
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    MOV DL, b
+    INT 21H
+    MOV DL, "*"
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+
+    MOV AH, 9
+    LEA DX, NL
+    INT 21H
+
+    MOV AH, 2
+    MOV DL, "*"
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    MOV DL, c
+    INT 21H
+    MOV DL, "*"
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+    INT 21H
+
+    MOV AH, 9
+    LEA DX, NL
+    INT 21H
+
+    MOV CX, 4
+    MOV AH, 9
+label2:
+    LEA DX, STAR
+    INT 21H
+    loop label2
+
+    
+    MOV AH, 4CH
+    INT 21H
+
+ENDP MAIN
+END MAIN
---
+++ Codes/Microprocessor/Assembly/Assignments/LAB2/Task2.asm
@@ -0,0 +1,28 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+
+
+MAIN PROC
+    MOV AH, 2
+    MOV CX, 10
+
+label1:
+    MOV BL, 10
+    label2:
+        MOV DL, "*"
+        INT 21H
+        SUB BL, 1
+        jnz label2
+
+        MOV DL, 0AH
+        INT 21H
+        MOV DL, 0DH
+        INT 21H
+    loop label1
+
+    MOV AH, 4CH
+    INT 21H
+        
+ENDP MAIN
+END MAIN
