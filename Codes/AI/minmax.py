eed3e7d155b11b5b7dae267dc91ff7129ef82589
AHNayef <ahnayef@outlook.com>
2025-04-21 02:00:43 +0600

    Add AI algorithms: DLS, IDS, BFS, DFS, and Minimax implementations; update .gitignore for AI files

---
+++ Codes/AI/minmax.py
@@ -0,0 +1,37 @@
+import math
+
+
+def minmax(currentDepth, currentNode, maxTurn, score, targetDepth):
+    if currentDepth == targetDepth:
+        return score[currentNode]
+
+    if maxTurn:
+        return max(minmax(currentDepth+1, currentNode*2, False,score, targetDepth), 
+                   minmax(currentDepth+1, currentNode*2+1, False, score, targetDepth))
+    else:
+        return min(minmax(currentDepth+1, currentNode*2, True, score, targetDepth), 
+                   minmax(currentDepth+1, currentNode*2+1, True, score, targetDepth))
+
+
+
+
+score = []
+
+x = int(input("Enter the number of leaf node: "))
+for i in range(x):
+    score.append(int(input("Enter the score of leaf node: ")))
+
+
+totalDepth = math.log2(len(score))
+
+currentDepth = 0
+startingNode = int(input("Enter the starting node: "))
+
+maxTurn = True
+
+print("The score of the leaf node is: ", end = "")
+
+answer = minmax(currentDepth, startingNode, maxTurn, score, totalDepth)
+
+print("The best score is: ", answer)
+
