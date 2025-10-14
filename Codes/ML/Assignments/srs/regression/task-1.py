825e76197d1fe15582fa4b48eec0cb5a7e9c2bb3
AHN <ahndo7059@gmail.com>
2025-10-14 23:47:11 +0600

    SRS

--- Codes/ML/Assignments/srs/regression/task-1.py
+++
@@ -1,9 +0,0 @@
-# Predict Field Of Study from occupation
-
-# Data Looks Like:
-# Field of Study,Current Occupation,Age,Gender,Years of Experience,Education Level,Industry Growth Rate,Job Satisfaction,Work-Life Balance,Job Opportunities,Salary,Job Security,Career Change Interest,Skills Gap,Family Influence,Mentorship Available,Certifications,Freelancing Experience,Geographic Mobility,Professional Networks,Career Change Events,Technology Adoption,Likely to Change Occupation
-# Medicine,Business Analyst,48,Male,7,High School,High,7,10,83,198266,8,0,8,High,0,0,0,1,2,0,1,0
-# Education,Economist,44,Male,26,Master's,Low,10,3,55,96803,9,0,3,Medium,0,0,1,1,2,1,9,0
-# Education,Biologist,21,Female,27,Master's,Low,8,3,78,65920,4,0,4,Low,0,0,0,0,2,1,2,0
-# Education,Business Analyst,33,Male,14,PhD,Medium,7,9,62,85591,5,0,2,Medium,1,0,0,0,9,0,1,0
-# Arts,Doctor,28,Female,0,PhD,Low,3,1,8,43986,3,0,5,Low,0,0,1,0,2,0,7,1
