72c1afe3ee1b60787fc41083f652c7679381a36e
AHN <ahndo7059@gmail.com>
2025-10-14 22:50:16 +0600

    SRS

---
+++ Codes/ML/Assignments/srs/classification/task-2.py
@@ -0,0 +1,13 @@
+# Predict if a product review is fake
+
+# Data Looks like:
+# category,rating,text,label
+# Home_and_Kitchen,5.0,"Love this!  Well made, sturdy, and very comfortable.  I love it!Very pretty",1
+# Home_and_Kitchen,5.0,"love it, a great upgrade from the original.  I've had mine for a couple of years",1
+# Home_and_Kitchen,5.0,This pillow saved my back. I love the look and feel of this pillow.,1
+# Home_and_Kitchen,1.0,"Missing information on how to use it, but it is a great product for the price!  I",1
+# Home_and_Kitchen,5.0,Very nice set. Good quality. We have had the set for two months now and have not been,1
+# Home_and_Kitchen,3.0,I WANTED DIFFERENT FLAVORS BUT THEY ARE NOT.,1
+# Home_and_Kitchen,5.0,They are the perfect touch for me and the only thing I wish they had a little more space.,1
+# Home_and_Kitchen,3.0,These done fit well and look great.  I love the smoothness of the edges and the extra,1
+# Home_and_Kitchen,5.0,"Great big numbers & easy to read, the only thing I didn't like is the size of the",1
