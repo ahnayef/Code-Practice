3a12c1336534df81d23ca8e63c7fde97e1cf758e
AHN <ahndo7059@gmail.com>
2026-02-24 11:21:48 +0600

    Brudeforce

---
+++ Codes/CS/bruteforce.js
@@ -0,0 +1,6 @@
+// Bruteforce attack 
+
+// alphanumeric characters
+const passChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const actualPassword = 'PassOf1';
+let generatedPassword = '';
